11573a896d29263da6782d87e931aa2c9145c62e
Thant Nay Lin <thantnaylin1301@gmail.com>
2023-09-06 18:55:22 +0800

    Add background image to home page

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,48 +1,51 @@
 import React from "react";
 import {Container, Nav, Navbar} from "react-bootstrap";
 import {LinkContainer} from "react-router-bootstrap";
-import {NavBrand} from "../../shared/styled-components";
+import {BackgroundImage, NavBrand} from "../../shared/styled-components";
 import {PiArticleNyTimesFill} from "react-icons/pi";
 import {RiContactsLine} from "react-icons/ri";
 
 const Header = () => {
     return (
         <header>
-            <Navbar expand='lg' bg='dark' data-bs-theme='dark'>
-                <Container fluid>
-                    <LinkContainer to='/'>
-                        <div className="ps-2 ps-md-5">
-                            <NavBrand>
-                                JIJI'S SPACE
-                            </NavBrand>
-                        </div>
-                    </LinkContainer>
-                    <Navbar.Toggle aria-controls='basic-navbar-nav'></Navbar.Toggle>
+            <BackgroundImage
+                $imageUrl='https://images.unsplash.com/photo-1583144584182-1717fab24b1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1200&q=100'>
+                <Navbar expand='lg' data-bs-theme='dark'>
+                    <Container fluid>
+                        <LinkContainer to='/'>
+                            <div className="ps-2 ps-md-5">
+                                <NavBrand>
+                                    JIJI'S SPACE
+                                </NavBrand>
+                            </div>
+                        </LinkContainer>
+                        <Navbar.Toggle aria-controls='basic-navbar-nav'></Navbar.Toggle>
 
-                    <Navbar.Collapse id='basic-navbar-nav' className='justify-content-end'>
-                        <Nav className="ml-auto">
-                            <LinkContainer to="/stories">
-                                <Nav.Link>
-                                    <div className="d-flex align-items-center justify-content-lg-between">
-                                        <PiArticleNyTimesFill/>
-                                        <div className='ps-2'>STORIES</div>
-                                    </div>
+                        <Navbar.Collapse id='basic-navbar-nav' className='justify-content-end'>
+                            <Nav className="ml-auto">
+                                <LinkContainer to="/stories">
+                                    <Nav.Link>
+                                        <div className="d-flex align-items-center justify-content-lg-between">
+                                            <PiArticleNyTimesFill/>
+                                            <div className='ps-2'>STORIES</div>
+                                        </div>
 
-                                </Nav.Link>
-                            </LinkContainer>
-                            <LinkContainer to="/about">
-                                <Nav.Link>
-                                    <div className="d-flex align-items-center justify-content-lg-between">
-                                        <RiContactsLine/>
-                                        <div className='ps-2'>ABOUT ME</div>
-                                    </div>
-                                </Nav.Link>
-                            </LinkContainer>
-                        </Nav>
-                    </Navbar.Collapse>
+                                    </Nav.Link>
+                                </LinkContainer>
+                                <LinkContainer to="/about">
+                                    <Nav.Link>
+                                        <div className="d-flex align-items-center justify-contentht-lg-between">
+                                            <RiContactsLine/>
+                                            <div className='ps-2'>ABOUT ME</div>
+                                        </div>
+                                    </Nav.Link>
+                                </LinkContainer>
+                            </Nav>
+                        </Navbar.Collapse>
 
-                </Container>
-            </Navbar>
+                    </Container>
+                </Navbar>
+            </BackgroundImage>
         </header>
     );
 }
--- src/shared/styled-components.tsx
+++ src/shared/styled-components.tsx
@@ -2,7 +2,16 @@ import styled from "styled-components";
 import {Navbar} from "react-bootstrap";
 
 export const NavBrand = styled(Navbar.Brand)`
-      font-size: 2rem;
-      font-weight: 500;
-      cursor: pointer;
-    `;
+  font-size: 2rem;
+  font-weight: 500;
+  cursor: pointer;
+`;
+
+export const BackgroundImage = styled.div<{ $imageUrl: string; }>`
+  width: 100%;
+  height: 50vh;
+  background-image: url(${props => props.$imageUrl || 'https://images.pexels.com/photos/5480787/pexels-photo-5480787.jpeg'});
+  background-position-y: center;
+  background-position-x: center;
+  background-size: cover;
+`

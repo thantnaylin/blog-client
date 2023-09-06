89311a9d924b0f3ea8327054ee8b916833663979
Thant Nay Lin <thantnaylin1301@gmail.com>
2023-09-06 17:39:46 +0800

    Add navbar

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,15 +1,46 @@
 import React from "react";
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import {Container, Nav, Navbar} from "react-bootstrap";
 import {LinkContainer} from "react-router-bootstrap";
+import {NavBrand} from "../../shared/styled-components";
+import {PiArticleNyTimesFill} from "react-icons/pi";
+import {RiContactsLine} from "react-icons/ri";
 
 const Header = () => {
     return (
         <header>
-            <Navbar bg='dark' data-bs-theme='dark'>
+            <Navbar expand='lg' bg='dark' data-bs-theme='dark'>
                 <Container fluid>
                     <LinkContainer to='/'>
-                        <Navbar.Brand className='ps-2 ps-md-5 brand-text'>Jiji's Space</Navbar.Brand>
+                        <div className="ps-2 ps-md-5">
+                            <NavBrand>
+                                JIJI'S SPACE
+                            </NavBrand>
+                        </div>
                     </LinkContainer>
+                    <Navbar.Toggle aria-controls='basic-navbar-nav'></Navbar.Toggle>
+
+                    <Navbar.Collapse id='basic-navbar-nav' className='justify-content-end'>
+                        <Nav className="ml-auto">
+                            <LinkContainer to="/stories">
+                                <Nav.Link>
+                                    <div className="d-flex align-items-center justify-content-lg-between">
+                                        <PiArticleNyTimesFill/>
+                                        <div className='ps-2'>STORIES</div>
+                                    </div>
+
+                                </Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/about">
+                                <Nav.Link>
+                                    <div className="d-flex align-items-center justify-content-lg-between">
+                                        <RiContactsLine/>
+                                        <div className='ps-2'>ABOUT ME</div>
+                                    </div>
+                                </Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+
                 </Container>
             </Navbar>
         </header>
---
+++ src/shared/styled-components.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import {Navbar} from "react-bootstrap";
+
+export const NavBrand = styled(Navbar.Brand)`
+      font-size: 2rem;
+      font-weight: 500;
+      cursor: pointer;
+    `;

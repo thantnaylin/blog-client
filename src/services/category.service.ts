ce3dcb737ec16e6b4fcde63af5b6bbbaea8f2e0a
Thant Nay Lin <thantnaylin1301@gmail.com>
2023-09-19 21:33:35 +0800

    create a service file

---
+++ src/services/category.service.ts
@@ -0,0 +1,6 @@
+import axios, { AxiosResponse } from "axios";
+import { ICategoryResult } from "./category.interfaces";
+
+// export const getCategories = async (): AxiosResponse<ICategoryResult> => {
+//     return axios.get<ICategoryResult>("http://localhost:1337/api/categories");
+// }

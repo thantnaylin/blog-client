6b85a8ff897dff8d8db218ca0161f376b660307b
Thant Nay Lin <thantnaylin1301@gmail.com>
2023-10-16 11:45:47 +0800

    Fix minor spacing issue

--- src/components/ui/About.tsx
+++ src/components/ui/About.tsx
@@ -8,38 +8,48 @@ export const About: React.FC = props => {
       <div className="d-flex mt-5 flex-column justify-content-center align-items-center">
         <div className="profile-picture-container"></div>
         <div className="personal-info px-md-5 mx-md-5 px-2 mx-2 py-5">
-          <h3>예비기자ㅣ허지혜</h3>
+          <h3 className="text-center">예비기자ㅣ허지혜</h3>
           <div className="row mt-5">
-            <div className="col-5">이름</div>
-            <div className="col-7">
+            <div className="col-12 col-md-6 text-md-start text-center">
+              이름
+            </div>
+            <div className="col-12 col-md-6 text-md-start text-center">
               <label className="fw-bold">허지혜</label>
             </div>
           </div>
 
           <div className="row mt-3">
-            <div className="col-5">생년월일</div>
-            <div className="col-7">
+            <div className="col-12 col-md-6 text-md-start text-center">
+              생년월일
+            </div>
+            <div className="col-12 col-md-6 text-md-start text-center">
               <label className="fw-bold">1997/05/12</label>
             </div>
           </div>
 
           <div className="row mt-3">
-            <div className="col-5">연락처</div>
-            <div className="col-7">
+            <div className="col-12 col-md-6 text-md-start text-center">
+              연락처
+            </div>
+            <div className="col-12 col-md-6 text-md-start text-center">
               <label className="fw-bold">010-2357-6205</label>
             </div>
           </div>
 
           <div className="row mt-3">
-            <div className="col-5">이메일</div>
-            <div className="col-7">
+            <div className="col-12 col-md-6 text-md-start text-center">
+              이메일
+            </div>
+            <div className="col-12 col-md-6 text-md-start text-center">
               <label className="fw-bold">ooiffyoff@gmail.com</label>
             </div>
           </div>
 
           <div className="row mt-3">
-            <div className="col-5">학력사항</div>
-            <div className="col-7">
+            <div className="col-12 col-md-6 text-md-start text-center">
+              학력사항
+            </div>
+            <div className="col-12 col-md-6 text-md-start text-center">
               <label className="fw-bold">
                 2022.02 건국대학교 현대미술학과 졸업 리빙디자인학과
               </label>
@@ -47,8 +57,10 @@ export const About: React.FC = props => {
           </div>
 
           <div className="row mt-3">
-            <div className="col-5">활동사항</div>
-            <div className="col-7">
+            <div className="col-12 col-md-6 text-md-start text-center">
+              활동사항
+            </div>
+            <div className="col-12 col-md-6 text-md-start text-center">
               <label className="fw-bold">
                 2022.10 한국잡지협회 취재기자 양성과정 25기 수료
               </label>
